76c9100ac045cc9ab58e5edcd8da6554551cfea5
Tomas Carvajal <tomyandrei@hotmail.com>
2021-05-04 18:32:38 -0400

    P4-006 Added configuration for 3 configuration files and created a new endpoint

---
+++ Practice3/Practice4/Controllers/InfoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice4.Controllers
+{
+    [ApiController]
+    [Route("/api/info")]
+    public class InfoController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public InfoController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public string getInfo()
+        {
+            string tmp = _config.GetSection("ProjectTitle").Value;
+            string env = _config.GetSection("EnvironmentName").Value;
+            string conn = _config.GetConnectionString("DataSource");
+            Console.WriteLine(conn);
+            return tmp + env;
+        }
+    }
+}
---
+++ Practice3/Practice4/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+         .SetBasePath(env.ContentRootPath)
+         .AddJsonFile("appsettings.json")
+         .AddJsonFile($"appsettings.{env.EnvironmentName}.json")
+         .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
